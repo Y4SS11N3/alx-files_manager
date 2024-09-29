849b42aa27a5eb5cd171c9e4d0fe0d316bc94eed
YASSINE <contact@yassine.fun>
2024-09-29 16:00:24 +0000

    Get and list file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/', routes);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
